84e4dd212c25779d22b2f086699dfcfea9ca15dc
Dom Farr <dominicfarr@gmail.com>
2022-03-21 19:30:51 -0300

    Removed amplify for the second time due to bugs in service

--- src/App.js
+++ src/App.js
@@ -2,61 +2,51 @@ import "./App.css";
 import { Onboarding } from "./components/Onboarding/Onboarding";
 import { PinfluencerMUITheme } from "./theme";
 import { ThemeProvider } from "@mui/material/styles";
-import { Amplify } from "aws-amplify";
 
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-
-import awsExports from "./aws-exports";
-import { Button } from "@mui/material";
-import LogoutIcon from "@mui/icons-material/Logout";
-
-Amplify.configure(awsExports);
+// import { Amplify } from "aws-amplify";
+// import { Authenticator } from "@aws-amplify/ui-react";
+// import "@aws-amplify/ui-react/styles.css";
+// import awsExports from "./aws-exports";
+// Amplify.configure(awsExports);
+// import { Button } from "@mui/material";
+// import LogoutIcon from "@mui/icons-material/Logout";
 
 function App() {
   return (
-    <Authenticator socialProviders={["google"]}>
-      {({ signOut, user }) => (
-        <ThemeProvider theme={PinfluencerMUITheme}>
-          <div className="HolyGrail">
-            <header>
-              <div className="HolyGrailHeader" role="banner">
-                <div className="HolyGrailHeaderLogo">LOGO</div>
-                <div className="HolyGrailHeaderActions">
-                  <div className="HolyGrailHeaderActionsCenter">
-                    Header Actions
-                  </div>
-                  <div className="HolyGrailHeaderActionsRight">
-                    {
-                      <Button onClick={signOut} endIcon={<LogoutIcon />}>
-                        Sign out
-                      </Button>
-                    }
-                  </div>
-                </div>
+    // <Authenticator socialProviders={["google"]}> {({ signOut, user }) => (
+    <ThemeProvider theme={PinfluencerMUITheme}>
+      <div className="HolyGrail">
+        <header>
+          <div className="HolyGrailHeader" role="banner">
+            <div className="HolyGrailHeaderLogo">LOGO</div>
+            <div className="HolyGrailHeaderActions">
+              <div className="HolyGrailHeaderActionsCenter">Header Actions</div>
+              <div className="HolyGrailHeaderActionsRight">
+                {/* { <Button onClick={signOut} endIcon={<LogoutIcon />}>Sign out</Button>} */}
               </div>
-            </header>
-            <main className="HolyGrailBody">
-              <article className="HolyGrailContent">
-                {/* <Onboarding user={user} /> */}
-                <Onboarding />
-              </article>
-              <nav className="HolyGrailNav">
-                <span>Navigation</span>
-              </nav>
-              <aside className="HolyGrailSidebar">
-                <span>Sidebar</span>
-              </aside>
-            </main>
-            <footer className="HolyGrailFooter">
-              <p>Footer items</p>
-              <p>&copy; 2022 Pinfluencer</p>
-              <p>Social links</p>
-            </footer>
+            </div>
           </div>
-        </ThemeProvider>
-      )}
-    </Authenticator>
+        </header>
+        <main className="HolyGrailBody">
+          <article className="HolyGrailContent">
+            {/* <Onboarding user={user} /> */}
+            <Onboarding />
+          </article>
+          <nav className="HolyGrailNav">
+            <span>Navigation</span>
+          </nav>
+          <aside className="HolyGrailSidebar">
+            <span>Sidebar</span>
+          </aside>
+        </main>
+        <footer className="HolyGrailFooter">
+          <p>Footer items</p>
+          <p>&copy; 2022 Pinfluencer</p>
+          <p>Social links</p>
+        </footer>
+      </div>
+    </ThemeProvider>
+    // )}  </Authenticator>
   );
 }
 
