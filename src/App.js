2c8f4050e5dc2c057a29c1eba051c024d32328c1
Dom Farr <dominicfarr@gmail.com>
2022-03-21 21:33:41 -0300

    Auth added

--- src/App.js
+++ src/App.js
@@ -7,46 +7,54 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
+import { Button } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 Amplify.configure(awsExports);
-// import { Button } from "@mui/material";
-// import LogoutIcon from "@mui/icons-material/Logout";
 
 function App() {
   return (
-    // <Authenticator socialProviders={["google"]}> {({ signOut, user }) => (
-    <ThemeProvider theme={PinfluencerMUITheme}>
-      <div className="HolyGrail">
-        <header>
-          <div className="HolyGrailHeader" role="banner">
-            <div className="HolyGrailHeaderLogo">LOGO</div>
-            <div className="HolyGrailHeaderActions">
-              <div className="HolyGrailHeaderActionsCenter">Header Actions</div>
-              <div className="HolyGrailHeaderActionsRight">
-                {/* { <Button onClick={signOut} endIcon={<LogoutIcon />}>Sign out</Button>} */}
+    <Authenticator socialProviders={["google"]}>
+      {({ signOut, user }) => (
+        <ThemeProvider theme={PinfluencerMUITheme}>
+          <div className="HolyGrail">
+            <header>
+              <div className="HolyGrailHeader" role="banner">
+                <div className="HolyGrailHeaderLogo">LOGO</div>
+                <div className="HolyGrailHeaderActions">
+                  <div className="HolyGrailHeaderActionsCenter">
+                    Header Actions
+                  </div>
+                  <div className="HolyGrailHeaderActionsRight">
+                    {
+                      <Button onClick={signOut} endIcon={<LogoutIcon />}>
+                        Sign out
+                      </Button>
+                    }
+                  </div>
+                </div>
               </div>
-            </div>
+            </header>
+            <main className="HolyGrailBody">
+              <article className="HolyGrailContent">
+                <Onboarding user={user} />
+                {/* <Onboarding /> */}
+              </article>
+              <nav className="HolyGrailNav">
+                <span>Navigation</span>
+              </nav>
+              <aside className="HolyGrailSidebar">
+                <span>Sidebar</span>
+              </aside>
+            </main>
+            <footer className="HolyGrailFooter">
+              <p>Footer items</p>
+              <p>&copy; 2022 Pinfluencer</p>
+              <p>Social links</p>
+            </footer>
           </div>
-        </header>
-        <main className="HolyGrailBody">
-          <article className="HolyGrailContent">
-            {/* <Onboarding user={user} /> */}
-            <Onboarding />
-          </article>
-          <nav className="HolyGrailNav">
-            <span>Navigation</span>
-          </nav>
-          <aside className="HolyGrailSidebar">
-            <span>Sidebar</span>
-          </aside>
-        </main>
-        <footer className="HolyGrailFooter">
-          <p>Footer items</p>
-          <p>&copy; 2022 Pinfluencer</p>
-          <p>Social links</p>
-        </footer>
-      </div>
-    </ThemeProvider>
-    // )}  </Authenticator>
+        </ThemeProvider>
+      )}
+    </Authenticator>
   );
 }
 
