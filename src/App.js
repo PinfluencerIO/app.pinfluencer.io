58ec4ee428638aaef33415ca494b8e5f068bc843
Dom Farr <dominicfarr@gmail.com>
2022-03-21 23:35:08 -0300

    Added google login with custom domain for auth

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 import { Button } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
+
+if (awsExports.aws_user_pools_id === "eu-west-2_pAnecv0rz") {
+  awsExports.oauth.domain = "pinfluencer-dev.auth.eu-west-2.amazoncognito.com";
+}
+
 Amplify.configure(awsExports);
 
 function App() {
