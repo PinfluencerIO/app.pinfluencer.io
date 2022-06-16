ef857e95b78c33b3298fde2e210cdb4bc42f784b
Dom Farr <dominicfarr@gmail.com>
2022-06-16 16:08:01 -0300

    some fun with mui avatar

--- src/App.js
+++ src/App.js
@@ -54,9 +54,11 @@ function App() {
             path="*"
             element={
               <main style={{ padding: "1rem" }}>
+                <p>There&apos;s nothing here! </p>
+                <p>The feature you wanted might not be ready yet </p>
                 <p>
-                  There&apos;s nothing here! Not to worry, click the Pinfluencer
-                  logo to go to the home page 😉
+                  Not to worry, click the Pinfluencer logo to go to the home
+                  page 😉
                 </p>
               </main>
             }
--- src/components/layout/Topbar.js
+++ src/components/layout/Topbar.js
@@ -1,10 +1,39 @@
 import { useContext } from "react";
-
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 import UserContext from "../../context/UserContext";
 import LogoHomeLink from "../LogoHomeLink";
 
 export default function Topbar() {
   const { user, signin, signout } = useContext(UserContext);
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    };
+  }
   return (
     <header className="page-header">
       <div>
@@ -13,9 +42,12 @@ export default function Topbar() {
       <h3> Header Component </h3>
       <div>
         {user ? (
-          <div>
-            {user.given_name}
-            {", "}
+          <Stack direction="row" spacing={2}>
+            <Avatar
+              {...stringAvatar(user.given_name + " " + user.family_name)}
+              src={user.picture}
+              sx={{ width: 56, height: 56 }}
+            />
             <button
               onClick={() => {
                 localStorage.setItem("signout", true);
@@ -24,9 +56,12 @@ export default function Topbar() {
             >
               Sign Out
             </button>
-          </div>
+          </Stack>
         ) : (
-          <button onClick={() => signin()}>Sign In</button>
+          <Stack direction="row" spacing={2}>
+            <Avatar sx={{ width: 56, height: 56 }} />
+            <button onClick={() => signin()}>Sign In</button>
+          </Stack>
         )}
       </div>
     </header>
