08220b77ea04c7160bc191bbc3b31d65749b8a26
Dom Farr <dominicfarr@gmail.com>
2022-07-02 09:37:17 -0300

    deleted over layout for view campaign moved to mui grid

--- src/pages/campaigns/ViewCampaign.js
+++ src/pages/campaigns/ViewCampaign.js
@@ -1,9 +1,17 @@
-import { Button, Chip, Paper, Stack, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import {
+  Box,
+  Button,
+  Chip,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router";
 import { getCampaign } from "../../api/api";
+import { OBJECTIVES } from "../../api/data";
 import isValidUUID from "../../components/uuidUtils";
 import { BadUrl } from "../BadUrl";
 
@@ -18,89 +26,142 @@ export const ViewCampaign = () => {
   if (!campaignId) {
     return <BadUrl />;
   }
-
+  false &&
+    console.log(nav, campaign, Button, Chip, Paper, Stack, Typography, Box);
   return (
-    <Stack spacing={2}>
-      <Box display="flex" flexDirection="row" justifyContent="end">
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => {
-            nav("edit");
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={12} md={8}>
+        <Paper
+          sx={{
+            padding: "24px",
+            "& p:not(:first-child)": { mt: "10px" },
           }}
         >
-          Edit
-        </Button>
-      </Box>
-      <Stack spacing={2} direction="row">
-        <Paper sx={{ flexGrow: 1 }}>
-          <Typography component="h3" sx={{ color: "lightText" }}>
-            Campaign Details
+          <Typography sx={{ color: "lightText" }}>Campaign Details</Typography>
+          <Typography sx={{ fontSize: "1.5rem" }}>
+            {campaign?.campaignTitle}
           </Typography>
-          <Typography variant="h4">{campaign?.campaignTitle}</Typography>
-          <Typography variant="h5">Campaign Details</Typography>
+          <Typography sx={{}}>Description</Typography>
           <Typography sx={{ color: "lightText" }}>
-            {/* {campaign?.campaignDescription} */}
+            {campaign?.campaignDescription}
           </Typography>
-          <Stack direction="row" spacing={10}>
-            <Stack>
-              <Typography variant="h5">Product Link</Typography>
-              <Typography sx={{ color: "lightText" }} component="span">
-                {campaign?.campaignProductLink}
-              </Typography>
-            </Stack>
-            <Stack>
-              <Typography variant="h5">Discount code</Typography>
-              <Typography sx={{ color: "lightText" }} component="span">
-                {campaign?.campaignDiscountCode}
-              </Typography>
-            </Stack>
-            <Stack>
-              <Typography variant="h5">Hashtag</Typography>
-              <Typography sx={{ color: "lightText" }} component="span">
-                {campaign?.campaignHashtag}
-              </Typography>
-            </Stack>
-          </Stack>
-          <Typography component="h3" sx={{ color: "lightText" }}>
-            Categories
-          </Typography>
-          {campaign?.campaignCategories.map((c) => {
-            return (
-              <Chip
-                key={c}
-                label={c}
-                sx={{
-                  m: "10px",
-                  border: "1px solid ",
-                }}
-              />
-            );
-          })}
-          <Typography component="h3" sx={{ color: "lightText" }}>
-            Values
-          </Typography>
-          {campaign?.campaignValues.map((v) => {
-            return (
-              <Chip
-                key={v}
-                label={v}
-                sx={{
-                  m: "10px",
-                  border: "1px solid ",
-                }}
-              />
-            );
-          })}
         </Paper>
-        <Paper sx={{ flexGrow: 2 }}>
-          <Typography component="h3" sx={{ color: "lightText" }}>
+      </Grid>
+      <Grid item xs={12} sm={12} md={4}>
+        <Paper
+          sx={{
+            padding: "24px",
+            "& p:not(:first-child)": { mt: "10px" },
+          }}
+        >
+          <Typography component="p" sx={{ color: "lightText" }}>
             Campaign Objectives
           </Typography>
-          <Typography variant="h4">{campaign?.campaignTitle}</Typography>
+          <Typography sx={{ fontSize: "1.5rem" }}>
+            {OBJECTIVES.find((o) => o.key === campaign?.objective)?.label}
+          </Typography>
+          <Typography component="p">What does success look like?</Typography>
+          <Typography sx={{ color: "lightText" }}>
+            {campaign?.successDescription}
+          </Typography>
         </Paper>
-      </Stack>
-      <Paper>Product</Paper>
-    </Stack>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper
+          sx={{
+            padding: "24px",
+            "& p:not(:first-child)": { mt: "10px" },
+          }}
+        >
+          <Typography sx={{ color: "lightText" }}>Product</Typography>
+        </Paper>
+      </Grid>
+    </Grid>
   );
+
+  // (
+
+  // <Stack spacing={2} border={1}>
+  //   <Box display="flex" flexDirection="row" justifyContent="end">
+  //     <Button
+  //       variant="contained"
+  //       color="secondary"
+  //       onClick={() => {
+  //         nav("edit");
+  //       }}
+  //     >
+  //       Edit
+  //     </Button>
+  //   </Box>
+  //   <Stack spacing={2} direction="row" border={1}>
+  //     <Paper sx={{ padding: "24px", lineHeight: "1rem" }}>
+  //       <Typography sx={{ color: "lightText" }}>Campaign Details</Typography>
+  //       <Typography>{campaign?.campaignTitle}</Typography>
+  //       <Typography>Campaign Details</Typography>
+  //       <Box overflow="auto">
+  //         <Typography sx={{ color: "lightText" }}>
+  //           {campaign?.campaignDescription}
+  //         </Typography>
+  //       </Box>
+  //       <Stack direction="row" spacing={10}>
+  //         <Stack>
+  //           <Typography>Product Link</Typography>
+  //           <Typography sx={{ color: "lightText" }} component="span">
+  //             {campaign?.campaignProductLink}
+  //           </Typography>
+  //         </Stack>
+  //         <Stack>
+  //           <Typography>Discount code</Typography>
+  //           <Typography sx={{ color: "lightText" }} component="span">
+  //             {campaign?.campaignDiscountCode}
+  //           </Typography>
+  //         </Stack>
+  //         <Stack>
+  //           <Typography>Hashtag</Typography>
+  //           <Typography sx={{ color: "lightText" }} component="span">
+  //             {campaign?.campaignHashtag}
+  //           </Typography>
+  //         </Stack>
+  //       </Stack>
+  //       <Typography component="p" sx={{ color: "lightText", mt: "24px" }}>
+  //         Categories
+  //       </Typography>
+  //       {campaign?.campaignCategories.map((c) => {
+  //         return (
+  //           <Chip
+  //             key={c}
+  //             label={c}
+  //             sx={{
+  //               m: "10px",
+  //               border: "1px solid ",
+  //             }}
+  //           />
+  //         );
+  //       })}
+  //       <Typography component="p" sx={{ color: "lightText" }}>
+  //         Values
+  //       </Typography>
+  //       {campaign?.campaignValues.map((v) => {
+  //         return (
+  //           <Chip
+  //             key={v}
+  //             label={v}
+  //             sx={{
+  //               m: "10px",
+  //               border: "1px solid ",
+  //             }}
+  //           />
+  //         );
+  //       })}
+  //     </Paper>
+  //     <Paper sx={{ padding: "24px" }}>
+  //       <Typography component="p" sx={{ color: "lightText" }}>
+  //         Campaign Objectives
+  //       </Typography>
+  //       <Typography>{campaign?.campaignTitle}</Typography>
+  //     </Paper>
+  //   </Stack>
+  //   <Paper sx={{ padding: "24px" }}>Product</Paper>
+  // </Stack>
+  // );
 };
