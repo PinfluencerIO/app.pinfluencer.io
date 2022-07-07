1145189a1902cce25e5c56bd9f91a26ff77ff5d5
Dom Farr <dominicfarr@gmail.com>
2022-07-07 13:45:48 +0100

    Added delete icon button to product image...no endpoint available at this time

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,7 +1,7 @@
 import { IconButton, Stack, Typography } from "@mui/material";
 import React, { Fragment, useState } from "react";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
-
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 export const ImageUpload = ({
   data,
   elementId,
@@ -13,29 +13,47 @@ export const ImageUpload = ({
 
   return (
     <Fragment>
-      <IconButton
-        onClick={() => document.getElementById(elementId).click()}
-        aria-label="logo image upload"
-        sx={{
-          border: "1px solid",
-          borderRadius: "5px",
-          background: " url(" + imageSrc + ") center center no-repeat",
-          backgroundSize: "contain",
-        }}
-      >
-        <Stack spacing={1} sx={{ alignItems: "center" }}>
-          <FileUploadIcon sx={{ fontSize: "5rem" }} />
-          <Typography component="p">{label}</Typography>
-          <Typography component="p">{sizeLabel}</Typography>
-        </Stack>
-      </IconButton>
-      <input
-        type="file"
-        style={{ margin: "-20px 0", visibility: "hidden" }}
-        name={elementId}
-        id={elementId}
-        onChange={() => previewImage(elementId, setImageSrc, tooLargeAlert)}
-      ></input>
+      <Stack>
+        <IconButton
+          onClick={() => document.getElementById(elementId).click()}
+          aria-label="upload"
+          sx={{
+            flexGrow: 1,
+            border: "1px solid",
+            borderRadius: "5px",
+            background: " url(" + imageSrc + ") center center no-repeat",
+            backgroundSize: "contain",
+          }}
+        >
+          <Stack spacing={1} sx={{ alignItems: "center" }}>
+            <FileUploadIcon sx={{ fontSize: "5rem" }} />
+            <Typography component="p">{label}</Typography>
+            <Typography component="p">{sizeLabel}</Typography>
+          </Stack>
+        </IconButton>
+
+        <IconButton aria-label="delete image" color="red">
+          <HighlightOffIcon
+            onClick={() => {
+              document.getElementById(elementId).value = null;
+              setImageSrc(null);
+            }}
+            sx={{
+              visibility:
+                imageSrc && !imageSrc.includes("default_product_image")
+                  ? "visible"
+                  : "hidden",
+            }}
+          />
+        </IconButton>
+        <input
+          type="file"
+          style={{ margin: "-20px 0", visibility: "hidden" }}
+          name={elementId}
+          id={elementId}
+          onChange={() => previewImage(elementId, setImageSrc, tooLargeAlert)}
+        ></input>
+      </Stack>
     </Fragment>
   );
   function previewImage(elementId, updateStateFunction, tooLargeAlert) {
